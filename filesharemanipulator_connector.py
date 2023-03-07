3b013c3fe4ae83cfc981540b008448ca59e645ae
patrykdros97 <102264923+patrykdros97@users.noreply.github.com>
2023-03-07 01:15:17 -0800

    PAPP-29500: File Share Manipulator - New App (#1)
    * File Share Manipulator app adding to repo
    
    * adding contributors and changing app version into developer
    
    * update app version and abilities
    
    * dev best practices changes, added dependend libraries, update json
    
    * static job changes
    
    * change min phantom version
    
    * Update action type, add fips compliant
    
    * update documentation
    
    ---------
    
    Co-authored-by: pdros <pdros@splunk.com>
    Co-authored-by: aparekh-crest <aparekh@splunk.com>

---
+++ filesharemanipulator_connector.py
@@ -0,0 +1,400 @@
+# File: filesharemanipulator_connector.py
+#
+# Copyright (c) 2023 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+# Python 3 Compatibility imports
+from __future__ import print_function, unicode_literals
+
+# Usage of the consts file is recommended
+import getpass
+import json
+import os
+import sys
+
+# Phantom App imports
+import phantom.app as phantom
+import requests
+from bs4 import BeautifulSoup
+from impacket.smbconnection import SMBConnection
+from phantom import vault
+from phantom.action_result import ActionResult
+from phantom.base_connector import BaseConnector
+
+from filesharemanipulator_consts import *
+
+
+class RetVal(tuple):
+    def __new__(cls, val1, val2=None):
+        return tuple.__new__(RetVal, (val1, val2))
+
+
+class FileShareManipulatorConnector(BaseConnector):
+
+    def __init__(self):
+
+        # Call the BaseConnectors init first
+        super(FileShareManipulatorConnector, self).__init__()
+
+        self._state = None
+        self._container_id = None
+        self.app_json = None
+
+        # Variable to hold a base_url in case the app makes REST calls
+        # Do note that the app json defines the asset config, so please
+        # modify this as you deem fit.
+        self._base_url = None
+
+    def _process_empty_response(self, response, action_result):
+        if response.status_code == [200, 204]:
+            return RetVal(phantom.APP_SUCCESS, {})
+
+        return RetVal(
+            action_result.set_status(
+                phantom.APP_ERROR, "Empty response and no information in the header"
+            ), None
+        )
+
+    def _process_html_response(self, response, action_result):
+        status_code = response.status_code
+
+        try:
+            soup = BeautifulSoup(response.text, "html.parser")
+            # Remove the script, style, footer and navigation part from the HTML message
+            for element in soup(["script", "style", "footer", "nav"]):
+                element.extract()
+            error_text = soup.text
+            split_lines = error_text.split('\n')
+            split_lines = [x.strip() for x in split_lines if x.strip()]
+            error_text = '\n'.join(split_lines)
+        except:
+            error_text = "Cannot parse error details"
+
+        message = "Status Code: {0}. Data from server:\n{1}\n".format(
+            status_code, error_text)
+
+        message = message.replace(u'{', '{{').replace(u'}', '}}')
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _process_json_response(self, r, action_result):
+        # Try a json parse
+        try:
+            resp_json = r.json()
+        except Exception as e:
+            error_message = self._get_error_message_from_exception(e)
+            return RetVal(
+                action_result.set_status(
+                    phantom.APP_ERROR, "Unable to parse JSON response. Error: {0}".format(
+                        error_message)
+                ), None
+            )
+
+        # Please specify the status codes here
+        if 200 <= r.status_code < 399:
+            return RetVal(phantom.APP_SUCCESS, resp_json)
+
+        # You should process the error returned in the json
+        message = "Error from server. Status Code: {0} Data from server: {1}".format(
+            r.status_code,
+            r.text.replace(u'{', '{{').replace(u'}', '}}')
+        )
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _process_response(self, r, action_result):
+        # store the r_text in debug data, it will get dumped in the logs if the action fails
+        if hasattr(action_result, 'add_debug_data'):
+            action_result.add_debug_data({'r_status_code': r.status_code})
+            action_result.add_debug_data({'r_text': r.text})
+            action_result.add_debug_data({'r_headers': r.headers})
+
+        # Process each 'Content-Type' of response separately
+
+        # Process a json response
+        if 'json' in r.headers.get('Content-Type', ''):
+            return self._process_json_response(r, action_result)
+
+        # Process an HTML response, Do this no matter what the api talks.
+        # There is a high chance of a PROXY in between phantom and the rest of
+        # world, in case of errors, PROXY's return HTML, this function parses
+        # the error and adds it to the action_result.
+        if 'html' in r.headers.get('Content-Type', ''):
+            return self._process_html_response(r, action_result)
+
+        # it's not content-type that is to be parsed, handle an empty response
+        if not r.text:
+            return self._process_empty_response(r, action_result)
+
+        # everything else is actually an error at this point
+        message = "Can't process response from server. Status Code: {0} Data from server: {1}".format(
+            r.status_code,
+            r.text.replace('{', '{{').replace('}', '}}')
+        )
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _dump_error_log(self, error, message="Exception occurred."):
+        self.error_print(message, dump_object=error)
+
+    def _get_error_message_from_exception(self, e):
+        """ This method is used to get appropriate error message from the exception.
+        :param e: Exception object
+        :return: error message
+        """
+
+        error_code = None
+        error_message = FILESHAREMANIPULATOR_ERROR_MESSAGE_UNAVAILABLE
+        self._dump_error_log(e)
+        try:
+            if hasattr(e, "args"):
+                if len(e.args) > 1:
+                    error_code = e.args[0]
+                    error_message = e.args[1]
+                elif len(e.args) == 1:
+                    error_message = e.args[0]
+        except Exception as ex:
+            self._dump_error_log(ex, "Error occurred while fetching exception information")
+
+        if not error_code:
+            error_text = "Error Message: {}".format(error_message)
+        else:
+            error_text = "Error Code: {}. Error Message: {}".format(error_code, error_message)
+
+        return error_text
+
+    def _make_rest_call(self, endpoint, action_result, method="get", **kwargs):
+        # **kwargs can be any additional parameters that requests.request accepts
+
+        config = self.get_config()
+
+        resp_json = None
+
+        try:
+            request_func = getattr(requests, method)
+        except AttributeError:
+            return RetVal(
+                action_result.set_status(
+                    phantom.APP_ERROR, "Invalid method: {0}".format(method)),
+                resp_json
+            )
+
+        # Create a URL to connect to
+        url = f'{self._base_url}{endpoint}'
+
+        try:
+            r = request_func(
+                url,
+                # auth=(username, password),  # basic authentication
+                verify=config.get('verify_server_cert', False),
+                **kwargs
+            )
+        except Exception as e:
+            error_message = self._get_error_message_from_exception(e)
+            return RetVal(
+                action_result.set_status(
+                    phantom.APP_ERROR, "Error Connecting to server. Details: {0}".format(
+                        error_message)
+                ), resp_json
+            )
+
+        return self._process_response(r, action_result)
+
+    def _handle_test_connectivity(self, param):
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        try:
+            client = SMBConnection(self._ip_address, self._ip_address)
+            client.login(self._username, self._password, self._domain)
+        except Exception as e:
+            error_message = self._get_error_message_from_exception(e)
+            self.save_progress(FILESHAREMANIPULATOR_ERROR_TEST_CONNECTIVITY)
+            self.save_progress(
+                "error: {}".format(error_message))
+            return action_result.get_status()
+
+        self.save_progress(FILESHAREMANIPULATOR_SUCCESS_TEST_CONNECTIVITY)
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _handle_get_file(self, param):
+        file_path = param['file_path']
+        share_name = param['share_name']
+        file_name = os.path.basename(file_path)
+        app_directory = self.app_json.get('directory')
+        app_version = self.app_json.get('app_version')
+        vault_path = os.path.join(f'/splunk_data/apps/{app_directory}/{app_version}', file_name)
+
+        if file_path[0] == '/':
+            file_path = file_path[1:]
+
+        self.save_progress("In action handler for: {0}".format(
+            self.get_action_identifier()))
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        try:
+            client = SMBConnection(self._ip_address, self._ip_address)
+            client.login(self._username, self._password, self._domain)
+            with open(vault_path, 'wb') as fh:
+                client.getFile(share_name, '\\' + file_path, fh.write)
+            client.close()
+
+        except Exception as e:
+            error_message = self._get_error_message_from_exception(e)
+            return action_result.set_status(phantom.APP_ERROR,
+                                            f'{"Error occurred while connection, Error: "}{error_message}')
+
+        succ, msg, vault_id = vault.vault_add(
+            container=self._container_id, file_location=vault_path, file_name=file_name)
+
+        if not succ:
+            return action_result.set_status(phantom.APP_ERROR, f'Error occurred while adding file to vault: {msg}')
+
+        action_result.add_data({'vault_id': vault_id})
+
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _handle_put_file(self, param):
+        path = param.get('path')
+        vault_id = param['vault_id']
+        share_name = param['share_name']
+
+        self.save_progress("In action handler for: {0}".format(
+            self.get_action_identifier()))
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        _, _, info = vault.vault_info(
+            vault_id=vault_id, container_id=self._container_id, trace=True)
+
+        try:
+            client = SMBConnection(self._ip_address, self._ip_address)
+            client.login(self._username, self._password, self._domain)
+            with open(info[0]['path'], 'rb') as fh:
+                client.putFile(share_name, os.path.join(
+                    path, info[0]['name']), fh.read)
+
+            client.close()
+
+        except Exception as e:
+            error_message = self._get_error_message_from_exception(e)
+            return action_result.set_status(phantom.APP_ERROR,
+                                            f'{"Error occurred while connection, Error: "}{error_message}')
+
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def handle_action(self, param):
+        ret_val = phantom.APP_SUCCESS
+
+        # Get the action that we are supposed to execute for this App Run
+        action_id = self.get_action_identifier()
+
+        self.debug_print("action_id", self.get_action_identifier())
+
+        if action_id == 'put_file':
+            ret_val = self._handle_put_file(param)
+
+        if action_id == 'get_file':
+            ret_val = self._handle_get_file(param)
+
+        if action_id == 'test_connectivity':
+            ret_val = self._handle_test_connectivity(param)
+
+        return ret_val
+
+    def initialize(self):
+        # Load the state in initialize, use it to store data
+        # that needs to be accessed across actions
+        self._state = self.load_state()
+        self._container_id = self.get_container_id()
+        self.app_json = self.get_app_json()
+
+        # get the asset config
+        config = self.get_config()
+
+        self._ip_address = config.get('ip_address')
+        self._username = config.get('username')
+        self._password = config.get('password')
+        self._domain = config.get('domain') if config.get('domain') else ''
+
+        return phantom.APP_SUCCESS
+
+    def finalize(self):
+        # Save the state, this data is saved across actions and app upgrades
+        self.save_state(self._state)
+        return phantom.APP_SUCCESS
+
+
+def main():
+    import argparse
+
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument('-v', '--verify', action='store_true', help='verify', required=False, default=False)
+
+    args = argparser.parse_args()
+    session_id = None
+
+    username = args.username
+    password = args.password
+    verify = args.verify
+
+    if username is not None and password is None:
+        # User specified a username but not a password, so ask
+        password = getpass.getpass("Splunk SOAR Password: ")
+
+    if username and password:
+        try:
+            login_url = BaseConnector._get_phantom_base_url() + 'login'
+
+            print("Accessing the Login page")
+            r = requests.get(login_url, verify=verify, timeout=FILESHAREMANIPULATOR_DEFAULT_TIMEOUT)
+            csrftoken = r.cookies['csrftoken']
+
+            data = dict()
+            data['username'] = username
+            data['password'] = password
+            data['csrfmiddlewaretoken'] = csrftoken
+
+            headers = dict()
+            headers['Cookie'] = 'csrftoken=' + csrftoken
+            headers['Referer'] = login_url
+
+            print("Logging into Platform to get the session id")
+            r2 = requests.post(login_url, verify=verify,
+                               data=data, headers=headers, timeout=FILESHAREMANIPULATOR_DEFAULT_TIMEOUT)
+            session_id = r2.cookies['sessionid']
+        except Exception as e:
+            print("Unable to get session id from the platform. Error: " + str(e))
+            sys.exit(1)
+
+    with open(args.input_test_json) as f:
+        in_json = f.read()
+        in_json = json.loads(in_json)
+        print(json.dumps(in_json, indent=4))
+
+        connector = FileShareManipulatorConnector()
+        connector.print_progress_message = True
+
+        if session_id is not None:
+            in_json['user_session_token'] = session_id
+            connector._set_csrf_info(csrftoken, headers['Referer'])
+
+        ret_val = connector._handle_action(json.dumps(in_json), None)
+        print(json.dumps(json.loads(ret_val), indent=4))
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
